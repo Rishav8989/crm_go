7e5e40fe285a69a06a1e60658098d6c117d61171
Michael Jäger <2405937+huNt-FMJ@users.noreply.github.com>
2022-11-08 23:41:10 +0100

    [createCustomer] Implement `createCustomer` & `go fmt`

--- main.go
+++ main.go
@@ -1,22 +1,50 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "github.com/gorilla/mux"
-  "strconv"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 type Customer struct {
-  ID int
-  Name string
-  Role string
-  Email string
-  Phone string
-  Contacted bool
+	ID        int
+	Name      string
+	Role      string
+	Email     string
+	Phone     string
+	Contacted bool
 }
 
+var database = map[int]Customer{
+	1: Customer{
+		ID:        1,
+		Name:      "Michael J",
+		Role:      "Student",
+		Email:     "Michael@udacity.com",
+		Phone:     "555-Nase",
+		Contacted: true,
+	},
+	2: Customer{
+		ID:        2,
+		Name:      "John Doe",
+		Role:      "Engineer",
+		Email:     "JohnD@udacity.com",
+		Phone:     "555-123456789",
+		Contacted: false,
+	},
+	3: Customer{
+		ID:        3,
+		Name:      "Jane Doe",
+		Role:      "Data Scientist",
+		Email:     "JaneD@udacity.com",
+		Phone:     "555-97654321",
+		Contacted: false,
+	},
+}
 
 var database = map[int]Customer {
    1: Customer{
@@ -69,14 +97,25 @@ func getCustomers(resp http.ResponseWriter, req *http.Request) {
   json.NewEncoder(resp).Encode(database)
 }
 
-
-func createCustomer(resp http.ResponseWriter, req *http.Request) {
-  resp.Header().Set("Content-type", "application/json")
-  fmt.Println("hitting createCustomer")
+func getCustomers(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-type", "application/json")
+	resp.WriteHeader(http.StatusOK)
+	json.NewEncoder(resp).Encode(database)
 }
 
+func createCustomer(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-type", "application/json")
 
-func updateCustomer(resp http.ResponseWriter, req *http.Request) {
+	bytes, _ := ioutil.ReadAll(req.Body)
+
+	customer := &Customer{}
+	if err := json.Unmarshal(bytes, customer); err != nil {
+		fmt.Println("Can't unmarshal given customer:", bytes)
+	}
+
+	addNewCustomer(customer)
+
+	json.NewEncoder(resp).Encode(database)
 }
 
 
@@ -84,22 +123,22 @@ func deleteCustomer(resp http.ResponseWriter, req *http.Request) {
 }
 
 func setupRouter() *mux.Router {
-  r := mux.NewRouter()
-  r.HandleFunc("/", getCustomers).Methods("GET")
-  r.HandleFunc("/customer/{id}", getCustomer).Methods("GET")
+	r := mux.NewRouter()
+	r.HandleFunc("/", getCustomers).Methods("GET")
+	r.HandleFunc("/customer/{id}", getCustomer).Methods("GET")
 
-  r.HandleFunc("/customer", updateCustomer).Methods("PUT")
+	r.HandleFunc("/customer", updateCustomer).Methods("PUT")
 
-  r.HandleFunc("/customer", createCustomer).Methods("POST")
-  r.HandleFunc("/customer/{id}", deleteCustomer).Methods("DELETE")
-  return r
+	r.HandleFunc("/customer", createCustomer).Methods("POST")
+	r.HandleFunc("/customer/{id}", deleteCustomer).Methods("DELETE")
+	return r
 }
 
 func main() {
-  
-  port := 3000
-  server_port := fmt.Sprintf(":%v", port)
 
-  fmt.Println("Server is available at localhost:", server_port)
-  http.ListenAndServe(server_port, setupRouter())
+	port := 3000
+	server_port := fmt.Sprintf(":%v", port)
+
+	fmt.Println("Server is available at localhost:", server_port)
+	log.Fatal(http.ListenAndServe(server_port, setupRouter()))
 }
