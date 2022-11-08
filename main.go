3146e3a334377e0acb2d1a164b4ff152b54aa35b
Michael Jäger <2405937+huNt-FMJ@users.noreply.github.com>
2022-11-08 23:57:38 +0100

    [customer struct] Annotate struct for JSON encoding/decoding

--- main.go
+++ main.go
@@ -11,12 +11,12 @@ import (
 )
 
 type Customer struct {
-	ID        int
-	Name      string
-	Role      string
-	Email     string
-	Phone     string
-	Contacted bool
+  ID        int 
+  Name      string `json: "name,omitempty"`
+	Role      string `json: "name,omitempty"`
+	Email     string `json: "name,omitempty"`
+	Phone     string `json: "name,omitempty"`
+	Contacted bool   `json: "name,omitempty"`
 }
 
 var database = map[int]Customer{
