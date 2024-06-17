41ebe1279fd9c335585b8a8c5ac9bad59a8bf8e0
Rishav8989 <betusenger@gmail.com>
2024-06-17 10:56:42 +0530

    json update

--- pkg/models/customer.go
+++ pkg/models/customer.go
@@ -1,10 +1,10 @@
 package models
 
 type Customer struct {
-	ID        int
-	Name      string `json: "name,omitempty"`
-	Role      string `json: "name,omitempty"`
-	Email     string `json: "name,omitempty"`
-	Phone     string `json: "name,omitempty"`
-	Contacted bool   `json: "name,omitempty"`
+	ID        int    `json:"id,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Role      string `json:"role,omitempty"`
+	Email     string `json:"email,omitempty"`
+	Phone     string `json:"phone,omitempty"`
+	Contacted bool   `json:"contacted,omitempty"`
 }
